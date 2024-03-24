905eb70f3e4ff6584860fca28f7e22237350ab7c
themathqueen <2497250a@research.gla.ac.uk>
2024-03-24 17:05:44 +0000

    nacgor

--- Monlib/LinearAlgebra/Nacgor.lean
+++ Monlib/LinearAlgebra/Nacgor.lean
@@ -3,9 +3,9 @@ Copyright (c) 2024 Monica Omar. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Monica Omar
 -/
-import Analysis.Normed.Group.Basic
-import Analysis.NormedSpace.PiLp
-import Analysis.InnerProductSpace.Basic
+import Mathlib.Analysis.Normed.Group.Basic
+import Mathlib.Analysis.NormedSpace.PiLp
+import Mathlib.Analysis.InnerProductSpace.Basic
 
 #align_import linear_algebra.nacgor
 
@@ -18,72 +18,65 @@ import Analysis.InnerProductSpace.Basic
 
 
 /-- `normed_add_comm_group` structure extended by `ring` -/
-@[class]
-structure NormedAddCommGroupOfRing (B : Type _) extends Norm B, Ring B, MetricSpace B where
-  dist := fun x y => ‖x - y‖
-  dist_eq : ∀ x y, dist x y = ‖x - y‖ := by obviously
+@[reducible]
+class NormedAddCommGroupOfRing (B : Type _) extends Ring B, NormedAddCommGroup B
 
 open scoped BigOperators
 
-def NormedAddCommGroupOfRing.toNormedAddCommGroup {B : Type _} [h : NormedAddCommGroupOfRing B] :
-    NormedAddCommGroup B where dist_eq := NormedAddCommGroupOfRing.dist_eq
+attribute [instance, reducible] NormedAddCommGroupOfRing.toNormedAddCommGroup
+attribute [instance, reducible] NormedAddCommGroupOfRing.toRing
+attribute [instance, reducible] NormedAddCommGroupOfRing.toNorm
 
-attribute [instance] NormedAddCommGroupOfRing.toNormedAddCommGroup
-
-attribute [instance] NormedAddCommGroupOfRing.toRing
-
+@[reducible]
 def Algebra.ofIsScalarTowerSmulCommClass {R A : Type _} [CommSemiring R] [Semiring A] [Module R A]
     [SMulCommClass R A A] [IsScalarTower R A A] : Algebra R A :=
   Algebra.ofModule smul_mul_assoc mul_smul_comm
 
 attribute [local instance] Algebra.ofIsScalarTowerSmulCommClass
 
-noncomputable instance Pi.normedAddCommGroupOfRing {ι : Type _} [Fintype ι] {B : ι → Type _}
-    [∀ i, NormedAddCommGroupOfRing (B i)] : NormedAddCommGroupOfRing (∀ i, B i)
+@[instance]
+noncomputable def PiNormedAddCommGroupOfRing {ι : Type _} [Fintype ι] {B : ι → Type _}
+    [Π i, NormedAddCommGroupOfRing (B i)] : NormedAddCommGroupOfRing (Π i, B i)
     where
-  toHasNorm := PiLp.instNorm 2 B
+  toNorm := PiLp.instNorm 2 B
   toRing := Pi.ring
-  toMetricSpace := PiLp.metricSpace 2 B
+  toMetricSpace := PiLp.instMetricSpacePiLp 2 B
   dist_eq x y := by
     have : 0 < (2 : ENNReal).toReal := by norm_num
     simp_rw [PiLp.norm_eq_sum this, PiLp.dist_eq_sum this, dist_eq_norm]
     rfl
 
-example {ι : Type _} [Fintype ι] {E : ι → Type _} [h : ∀ i, NormedAddCommGroupOfRing (E i)] :
+example {ι : Type _} [Fintype ι] {E : ι → Type _} [∀ i, NormedAddCommGroupOfRing (E i)] :
     @AddCommGroup.toAddCommMonoid _
-        (NormedAddCommGroupOfRing.toNormedAddCommGroup :
+        (PiNormedAddCommGroupOfRing.toNormedAddCommGroup :
             NormedAddCommGroup (∀ i, E i)).toAddCommGroup =
-      NonUnitalNonAssocSemiring.toAddCommMonoid _ :=
+      NonUnitalNonAssocSemiring.toAddCommMonoid :=
   rfl
 
--- set_option old_structure_cmd true
 theorem Pi.normedAddCommGroupOfRing.norm_eq_sum {ι : Type _} [Fintype ι] {B : ι → Type _}
-    [∀ i, NormedAddCommGroupOfRing (B i)] (x : ∀ i, B i) : ‖x‖ = Real.sqrt (∑ i, ‖x i‖ ^ 2) :=
+  [Π i, NormedAddCommGroupOfRing (B i)] (x : Π i, B i) :
+  PiNormedAddCommGroupOfRing.norm x = Real.sqrt (∑ i, ‖x i‖ ^ 2) :=
   by
   have : 0 < (2 : ENNReal).toReal := by norm_num
-  simp_rw [PiLp.norm_eq_sum this, ENNReal.toReal_bit0, ENNReal.one_toReal, Real.rpow_two,
-    Real.sqrt_eq_rpow]
+  simp_rw [PiLp.norm_eq_sum this, ENNReal.toReal_ofNat,
+    Real.rpow_two, Real.sqrt_eq_rpow]
 
-example {E : Type _} [h : NormedAddCommGroupOfRing E] :
-    @AddCommGroupWithOne.toAddCommGroup E (Ring.toAddCommGroupWithOne E) =
+example {E : Type _} [NormedAddCommGroupOfRing E] :
+    @Ring.toAddCommGroup E _ =
       NormedAddCommGroup.toAddCommGroup :=
   rfl
 
-example {E : Type _} [h : NormedAddCommGroupOfRing E] :
+example {E : Type _} [NormedAddCommGroupOfRing E] :
     (@AddCommGroup.toAddCommMonoid E NormedAddCommGroup.toAddCommGroup : AddCommMonoid E) =
-      (NonUnitalNonAssocSemiring.toAddCommMonoid E : AddCommMonoid E) :=
+      (NonUnitalNonAssocSemiring.toAddCommMonoid : AddCommMonoid E) :=
   rfl
 
 example {E : Type _} [Ring E] :
-    @AddCommGroup.toAddCommMonoid E
-        (@AddCommGroupWithOne.toAddCommGroup E (Ring.toAddCommGroupWithOne E)) =
-      NonUnitalNonAssocSemiring.toAddCommMonoid E :=
-  rfl
+  @AddCommGroup.toAddCommMonoid E (@Ring.toAddCommGroup E _) =
+    NonUnitalNonAssocSemiring.toAddCommMonoid :=
+rfl
 
 /-- `module` given by `algebra` is equal to that given by
-  `inner_product_space`, but they are not definitionally equal.
-  So this may cause some problems in the future.
-  In Lean 4, they are definitionally equal. -/
+  `inner_product_space`, but they are not definitionally equal. -/
 example {E : Type _} [NormedAddCommGroupOfRing E] [h : InnerProductSpace ℂ E] [SMulCommClass ℂ E E]
-    [IsScalarTower ℂ E E] : h.toModule = (Algebra.toModule : Module ℂ E) := by ext <;> rfl
-
+    [IsScalarTower ℂ E E] : h.toModule = (Algebra.toModule : Module ℂ E) := by ext; rfl
